b03772adcfd7585ee4f54e938c5961ade393e6c9
Bob Conlon <bob@monsoonco.com>
2020-08-08 11:23:55 -0700

    Adds initial test data

---
+++ src/app/testdata.ts
@@ -0,0 +1,59 @@
+
+export const testdata = [
+  {
+    category: "flash",
+    question: "Why did the chicken cross the road?",
+    answer: "To get from the left to the right, sir",
+    subNotes: ""
+  },
+  {
+    category: "flash",
+    question: "What is JavaScript?",
+    answer: "JavaScript is a client-side as well as server side scripting language that can be inserted into HTML pages and is understood by web browsers. JavaScript is also an Object based Programming language",
+    subNotes: ""
+  },
+  {
+    category: "choice",
+    choice: "4",
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer: [
+    { a: "<javascript>" },
+    { a: "<js>" },
+    { a: "<scripting>" },
+    { a: "<script>" },
+  ],
+    subNotes: ""
+  },
+  {
+    category: "choice",
+    choice: "2",
+    question: "What are JavaScript Data Types?",
+    answer: [
+      { a: "String, Boolean, Object, null, var" },
+      { a: "Number, String, Boolean, Object, Undefined" },
+      { a: "Number, String, Boolean, NaN, null" },
+      { a: "String, Boolean, Object, document, void", }
+    ],
+    subNotes: ""
+  },
+  {
+    category: "flash",
+    question: "What is the use of isNaN function?",
+    answer: "isNan function returns true if the argument is not a number otherwise it is false.",
+    subNotes: ""
+  },
+  {
+    category: "flash",
+    question: "What are undeclared and undefined variables?",
+    answer: "Undeclared variables are those that do not exist in a program and are not declared. Undefined variables are those that are declared in the program but have not been given any value.",
+    subNotes: "If the program tries to read the value of an undeclared variable, then a runtime error is encountered. If the program tries to read the value of an undefined variable, an undefined value is returned."
+  },
+  {
+    category: "flash",
+    question: "What is === operator?",
+    answer: "=== is called as strict equality operator which returns true when the two operands are having the same value without any type conversion.",
+    subNotes: ""
+  }
+]
+
+
